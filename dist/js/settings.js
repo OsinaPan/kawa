91895cc47c70277e5bc4b98af8e8380bfff6f6af
Michal Osinski <michal.osinski94@gmail.com>
2023-04-16 14:07:05 +0200

    add server.mjs

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -1,6 +1,6 @@
 const settings = {
   db: {
-    url: 'http://localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     products: 'products'
   }
 };
